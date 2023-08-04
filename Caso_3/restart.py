88f77023787d6c35c366729d7d1c5393443ec9bc
Agustinm28 <agustinm28@gmail.com>
2023-08-04 13:49:22 +0000

    caso_3

---
+++ Caso_3/restart.py
@@ -0,0 +1,32 @@
+import subprocess
+import argparse
+
+def delete():
+
+    # Borrar MySQL
+    subprocess.run(['kubectl','delete','sts','metabase-db']) # sts
+    subprocess.run(['kubectl','delete','pvc','mysql-data-metabase-db-0']) # pvc
+    subprocess.run(['kubectl','delete','service','mysql-service']) # service
+    subprocess.run(['kubectl','delete','secret','meta-db-secrets']) # secret
+    subprocess.run(['kubectl','delete','configmap','mysql-config']) # configmap
+
+    # Borrar Metabase
+    subprocess.run(['kubectl','delete','deploy','metabase']) # deployment
+    subprocess.run(['kubectl','delete','service','metabase-service']) # service
+    subprocess.run(['kubectl','delete','secret','metabase-secrets']) # secret
+    subprocess.run(['kubectl','delete','configmap','metabase-config']) # configmap 
+    #subprocess.run(['kubectl','delete','ingress','metabase-ingress']) # ingress 
+
+def restart():
+    delete()
+    subprocess.run(['kubectl','apply','-f','.'])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Delete or restart Metabase and MySQL')
+    parser.add_argument('-delete', action='store_true', help='Delete Metabase and MySQL', default=delete)
+    parser.add_argument('-restart', action='store_true', help='Restart Metabase and MySQL')
+    args = parser.parse_args()
+    if args.restart:
+        restart()
+    else:
+        delete()
